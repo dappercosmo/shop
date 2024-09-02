344087cfff5691d3e5323b664955cc46ea9eaeaa
dappercosmo <dappercosmo@gmail.com>
2024-09-02 15:03:05 +0530

    changes in Test class

--- src/test/java/com/prashanthSelections/shop/ShopApplicationTests.java
+++ src/test/java/com/prashanthSelections/shop/ShopApplicationTests.java
@@ -3,7 +3,7 @@ package com.prashanthSelections.shop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ShopApplication.class})
 class ShopApplicationTests {
 
 	@Test
